b68f8bc9b603b88e6355bfe100c670be3cc66424
Victor Anderssén <victor.anderssen@gmail.com>
2017-08-30 22:19:23 +0300

    Some parsing done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Campus Vasa lunch API  - Victor Anderssén 2017
 
+// Includes
 var express = require('express');
 var cors = require('cors');
 var request = require('request');
@@ -7,9 +8,11 @@ var bodyParser = require('body-parser')
 var prettyjson = require('prettyjson');
 var app = express();
 
+// Settings
 app.use(bodyParser.json());
 app.use(cors());
 
+// Routes / API endpoints
 app.get('/test', function(req, res){
   var alere = 'http://www.juvenes.fi/DesktopModules/Talents.LunchMenu/LunchMenuServices.asmx/GetMenuByWeekday?KitchenId=45&MenuTypeId=60&Week=35&Weekday=3&lang=%27sv-SE%27&format=json';
   var test = 'https://api.victoranderssen.com/lunchtoday/w33';
@@ -19,7 +22,15 @@ app.get('/test', function(req, res){
       
       var body = jsonClean(body);
 
-      console.log(prettyjson.render(body));
+      // is the var an object?
+      //console.log(body instanceof Object);
+
+      //that's all... no magic, no bloated framework
+      var lunchItemsSE = [];
+      traverse(body, process);
+      console.log(lunchItemsSE);
+
+      //console.log(prettyjson.render(keys));
       res.send(body);
     }
   })
@@ -31,12 +42,38 @@ function jsonClean(dirty) {
   var dirty = dirty.substr(7);
   // remove  "}); from the end
   var dirty = dirty.substr(0, dirty.length - 4);
-  // replace \ -> empty
-
-  var cleanJson = dirty.replace(/\\/g, ' ');
+  // replace all \ -> empty
+  var dirty = dirty.replace(/\\/g, '');
+  // convert back to valid json
+  var cleanJson = JSON.parse(dirty);
 
   return cleanJson;
 }
+
+
+//called with every property and its value
+function process(key, value) {
+  //console.log(key + " : "+ value);
+
+  // Find all occurances of the swedish lunch items that do not begin with LOUNAS ?! and are not empty ... API at its finest
+  if (key == 'Name_SV' && value.includes('LOUNAS') == false && value !== '') {
+    console.log(key + " : "+ value);
+  }
+
+}
+
+function traverse(o, func) {
+  for (var i in o) {
+    func.apply(this,[i,o[i]]);  
+    if (o[i] !== null && typeof(o[i]) == "object") {
+        //going one step down in the object tree!!
+        traverse(o[i], func);
+    }
+  }
+}
+
+
+
 
 app.listen(3000);
 console.log("The server is now running on port 3000.");
